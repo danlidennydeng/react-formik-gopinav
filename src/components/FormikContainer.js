985c56c2ec973ab4aa291a4d0f64cff21b3a3843
Deng <danli.deng@cgi.com>
2021-10-13 17:01:48 -0700

    Lesson 34, 35, 36, textarea, select, radio

--- src/components/FormikContainer.js
+++ src/components/FormikContainer.js
@@ -5,11 +5,32 @@ import FormikControl from './FormikControl'
 
 function FormikContainer() {
 
+  const dropdownOptions = [
+    { key: 'Select an option', value: '' },
+    { key: 'option 1', value: 'option1' },
+    { key: 'option 2', value: 'option2' },
+    { key: 'option 3', value: 'option3' }
+  ]
+
+  const radioOptions = [
+    
+    { key: 'option 1', value: 'rOption1' },
+    { key: 'option 2', value: 'roption2' },
+    { key: 'option 3', value: 'roption3' }
+  ]
+
   const initialValues = {
-    email: ''
+    email: '',
+    description: '',
+    selectOption: '',
+    radioOption: ''
   }
+
   const validationSchema = Yup.object({
-    email: Yup.string().required('Requird!')
+    email: Yup.string().required('Requird!'),
+    description: Yup.string().required('Requird!'),
+    selectOption: Yup.string().required('Requird!'),
+    radioOption: Yup.string().required('Requird!')
   })
   
   const onSubmit = values => console.log('Form Data', values)
@@ -23,6 +44,13 @@ function FormikContainer() {
       {formik => (
         <Form>
           <FormikControl control='input' type='email' label='Email' name='email' />
+
+          <FormikControl control='textarea' label='Description' name='description' />
+
+          <FormikControl control='select' label='Select a topic' name='selectOption' options={dropdownOptions} />
+
+          <FormikControl control='radio' label='Radio Topic' name='radioOption' options={radioOptions} />
+
           <button type='submit'>Submit</button>
         </Form>
       )}
--- src/components/FormikControl.js
+++ src/components/FormikControl.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import Input from './Input'
+import RadioButtons from './RadioButtons'
+import Select from './Select'
+import Textarea from './Textarea'
 
 function FormikControl(props) {
   const { control, ...rest } = props
@@ -8,8 +11,11 @@ function FormikControl(props) {
     case 'input':
       return <Input {...rest} />
     case 'textarea':
+      return <Textarea {...rest} />
     case 'select':
+      return <Select {...rest} />
     case 'radio':
+      return <RadioButtons {...rest} />
     case 'checkbox':
     case 'date':  
     default: 
